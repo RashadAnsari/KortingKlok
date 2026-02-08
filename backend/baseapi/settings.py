a3830192ff03f34bd4d9f897bcc9816b2b97321f
Rashad Ansari <rashad.ansari1996@gmail.com>
2026-02-08 19:45:48 +0100

    Remove celery results

--- backend/baseapi/settings.py
+++ backend/baseapi/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     # Third party apps
     "rest_framework",
     "drf_spectacular",
-    "django_celery_results",
     "drf_standardized_errors",
 ]
 
@@ -81,10 +80,6 @@ CELERY_TASK_REJECT_ON_WORKER_LOST = True
 CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
 CELERY_WORKER_CONCURRENCY = multiprocessing.cpu_count() * 2 + 1
 CELERY_BROKER_URL = env.str("CELERY_BROKER_URL", default="redis://localhost:6379")
-
-CELERY_RESULT_EXTENDED = True
-CELERY_RESULT_BACKEND = "django-db"
-CELERY_RESULT_EXPIRES = 60 * 60 * 24 * 30  # 1 month
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, "apps", "tmpls")
 
