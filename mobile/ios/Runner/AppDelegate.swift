39223cac5b5c3e898e88090c43ffb1f4f6dc2683
Rashad Ansari <rashad.ansari1996@gmail.com>
2026-02-17 20:36:48 +0100

    Fix url opening issue

--- mobile/ios/Runner/AppDelegate.swift
+++ mobile/ios/Runner/AppDelegate.swift
@@ -20,7 +20,7 @@ import UserNotifications
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)  
-    DispatchQueue.main.async { UIApplication.shared.registerForRemoteNotifications()}
+    DispatchQueue.main.async { UIApplication.shared.registerForRemoteNotifications() }
 
     guard let controller = window?.rootViewController as? FlutterViewController else { return }
     let channel = FlutterMethodChannel(
--- mobile/lib/screens/profile_screen.dart
+++ mobile/lib/screens/profile_screen.dart
@@ -54,9 +54,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
 
   Future<void> _openUrl(String url) async {
     final uri = Uri.parse(url);
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri, mode: LaunchMode.externalApplication);
-    }
+    await launchUrl(uri, mode: LaunchMode.externalApplication);
   }
 
   void _showChangeNameSheet(
