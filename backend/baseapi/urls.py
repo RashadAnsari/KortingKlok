6391c849a09c8097cb84cef0b0c0a49c7e27d30b
Rashad Ansari <rashad.ansari1996@gmail.com>
2026-02-21 13:14:26 +0100

    Disable swagger from release backend

--- backend/baseapi/urls.py
+++ backend/baseapi/urls.py
@@ -1,9 +1,9 @@
 from django.urls import include, path
 
-from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+# from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("v1/", include("apis.v1.urls")),
-    path("swagger-schema", SpectacularAPIView.as_view(), name="schema"),
-    path("swagger-ui", SpectacularSwaggerView.as_view(), name="swagger-ui"),
+    # path("swagger-schema", SpectacularAPIView.as_view(), name="schema"),
+    # path("swagger-ui", SpectacularSwaggerView.as_view(), name="swagger-ui"),
 ]
